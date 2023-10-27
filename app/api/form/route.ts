077158600a612f5805858821e2b915994cf71ba6
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2023-10-28 02:58:09 +0800

    tes form

---
+++ app/api/form/route.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+import { google } from 'googleapis'
+import { authenticate } from '@google-cloud/local-auth'
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+
+    try {
+        const formID = "19aFPBCL0IiLPdWdmO5Qcp1-sET4CRt-yoI_YZ-A_ttU"
+
+        const auth = await authenticate({
+            keyfilePath: path.join(__dirname, 'credentials.json'),
+            scopes: 'https://www.googleapis.com/auth/forms.body.readonly',
+        });
+        const forms = google.forms({
+            version: 'v1',
+            auth: auth,
+        });
+        const res = await forms.forms.get({formId: formID});
+        console.log(res.data);
+
+        return NextResponse.json(res.data)
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(error)
+    }
+    
+}
