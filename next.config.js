2718faa7a6a794e0d8fcd5b2c4923dc02f414071
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2023-10-28 03:28:12 +0800

    tes form

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@
 const nextConfig = {
     images: {
         domains: ['images.unsplash.com']
-    },
-    serverRuntimeConfig: {
-        PROJECT_ROOT: __dirname
     }
-    // webpack: (config) => {
-    //     config.resolve.alias = {
-    //         ...config.resolve.alias,
-    //         '__dirname': __dirname,
-    //       };
-    // }
 }
 
 module.exports = nextConfig
